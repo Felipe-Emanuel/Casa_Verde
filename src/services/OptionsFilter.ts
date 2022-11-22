f3df6082e8c29aa3307c5507aa5fbfa99a13bd1b
Felipe <felipesullivan50@gmail.com>
2022-11-22 10:40:23 -0300

    build fix

--- src/services/OptionsFilter.ts
+++ src/services/OptionsFilter.ts
@@ -1,7 +1,5 @@
 import plant from "../util/plant.json";
 
-const prices = plant.map(price => price.preco)
-
 export const NameFilter = () => {
   plant.sort((a: any, b: any) => {
     return a.name < b.name ? -1 : 1;
